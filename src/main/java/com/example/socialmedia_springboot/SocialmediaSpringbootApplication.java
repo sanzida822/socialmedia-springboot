3aa25ab25f40ac3cf370fedb77f623e63766de00
sanzida822 <sanzidasultana822@gmail.com>
2025-08-07 01:24:33 +0600

    added initial commit

---
+++ src/main/java/com/example/socialmedia_springboot/SocialmediaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmedia_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmediaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialmedia_springboot/SocialmediaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmedia_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
